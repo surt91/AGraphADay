d5fdf098d2b0cb232e029bf7477d8310bdc29844
Hendrik Schawe <surt91@gmail.com>
2016-12-27 15:20:11 +0100

    fixed missing quotes -- again

--- keys_and_secrets.py
+++ keys_and_secrets.py
@@ -1,6 +1,6 @@
 keys_and_secrets = {
-    consumer_key: '[fill me]',
-    consumer_secret: '[fill me]',
-    access_token_key: '[fill me]',
-    access_token_secret: '[fill me]'
+    "consumer_key": '[fill me]',
+    "consumer_secret": '[fill me]',
+    "access_token_key": '[fill me]',
+    "access_token_secret": '[fill me]'
 }
