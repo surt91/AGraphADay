6a69cbe605b9bec2f26d7c7930b1eb4b05d5dd20
Hendrik Schawe <surt91@gmail.com>
2017-10-22 18:05:56 +0200

    added trivial script for randomized calls and controlled runtime

---
+++ call_randomly.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# will generate a graph every 24 hours +- 6 hours
+# will also timeout the generation after 2h and restart
+
+while true; do
+    sleep $(echo "18*3600 + ( ($RANDOM / 32767) * (12*3600) )" | bc -l)s
+    until timeout 7200 python3 main.py; do
+        echo "killed ... retry"
+    done
+done
