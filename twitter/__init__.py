07aaa9f67a30026c0ece2870352f8eb54099464c
Hendrik Schawe <surt91@gmail.com>
2020-08-23 11:17:15 +0200

    log warning but continue without twitter tokens

--- twitter/__init__.py
+++ twitter/__init__.py
@@ -1,2 +1,18 @@
-from .helper import tweet_pic, get_my_handle, obtain_dm
-from .listener import answerMentions
+try:
+    from .helper import tweet_pic, get_my_handle, obtain_dm
+    from .listener import answerMentions
+    from .networks import ego_network
+except:
+    print("Twitter package is broken")
+    def tweet_pic(*args):
+        print("Twitter package is broken")
+    def get_my_handle(*args):
+        print("Twitter package is broken")
+        return "none"
+    def obtain_dm(*args):
+        print("Twitter package is broken")
+        return []
+    def answerMentions(*args):
+        print("Twitter package is broken")
+    def ego_network(*args):
+        print("Twitter package is broken")
