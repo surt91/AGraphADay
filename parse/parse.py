e8c35492c630b7d9b1c819aaf2664915633a7229
Hendrik Schawe <surt91@gmail.com>
2021-06-24 11:11:25 +0200

    partial scorer was too good finding similar letters

--- parse/parse.py
+++ parse/parse.py
@@ -3,5 +3,5 @@ from fuzzywuzzy import process
 
 
 def match(text, choices):
-    m = process.extractOne(text, choices, scorer=fuzz.partial_ratio)
+    m = process.extractOne(text, choices)
     return m
