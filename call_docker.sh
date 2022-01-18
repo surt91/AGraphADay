97137f8431ddec870d8cb21083cfba4215bf67b8
Hendrik Schawe <surt91@gmail.com>
2022-01-18 19:23:26 +0100

    try again if it fails

--- call_docker.sh
+++ call_docker.sh
@@ -2,11 +2,16 @@
 
 # will also timeout the generation after 1h and restart
 
-# before usage build with 
-# docker build . -t graph
+# before usage build with
+docker build . -t graph
 
-timeout 3600 docker run \
+until timeout 3600 docker run \
     -v $PWD/img:/AGraphADay/archive \
     -v $PWD/test:/AGraphADay/test \
     -v $PWD/keys_and_secrets.py:/AGraphADay/keys_and_secrets.py \
-    graph
+    graph; do
+    echo "killed ... retry"
+    sleep 300
+done
+
+
